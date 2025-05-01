c57c56591a2454c538cc5432f6f5657c287aabe4
devoleper-samreen <samreenmalik52292@gmail.com>
2025-05-01 20:03:58 +0530

    refactor registerUser function to improve user creation response handling

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -20,9 +20,34 @@ export const registerUser = async (req, res) => {
             })
         }
 
+        const user = await User.create({
+            name,
+            email,
+            password,
+            phone
+        })
+
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: 'User not created'
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: 'User created successfully',
+            user
+        })
+
 
 
     } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error'
+        })
 
     }
 }  
