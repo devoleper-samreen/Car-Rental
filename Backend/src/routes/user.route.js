9959865d091fb00949f871dc2a59ed39e7285720
devoleper-samreen <samreenmalik52292@gmail.com>
2025-05-02 19:44:28 +0530

    add verifyToken middleware to change-password route for enhanced security

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { registerUser, loginUser, changePassword } from '../controllers/user.controller.js';
+import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.post('/change-password', changePassword);
+router.post('/change-password', verifyToken, changePassword);
 
 export default router;
