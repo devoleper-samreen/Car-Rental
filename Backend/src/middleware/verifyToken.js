58dd77ee387a76be193e3a93a8948f30dae91cfd
devoleper-samreen <samreenmalik52292@gmail.com>
2025-05-02 19:43:22 +0530

    add verifyToken middleware for JWT authentication and user verification

---
+++ Backend/src/middleware/verifyToken.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export const verifyToken = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decoded) {
+            return res.status(401).json({
+                success: false,
+                message: 'Unauthorized or Token expired'
+            })
+        }
+
+        const user = await User.findById(decoded.id);
+        req.user = user;
+        next();
+
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({
+            success: false,
+            message: 'Unauthorized or Token expired'
+        })
+
+    }
+}
