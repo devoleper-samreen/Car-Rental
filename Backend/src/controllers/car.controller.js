624f351c15df10bc81202392a163b55c9beb9092
devoleper-samreen <samreenmalik52292@gmail.com>
2025-05-09 14:39:29 +0530

    feat: implement searchCars function with filtering, sorting, and pagination

--- Backend/src/controllers/car.controller.js
+++ Backend/src/controllers/car.controller.js
@@ -101,6 +101,16 @@ export const getCarById = async (req, res) => {
     }
 }
 
+// export const getAllCars = async (req, res) => {
+//     try {
+
+
+//     } catch (error) {
+
+//     }
+// }
+
+
 export const getCarImage = async (req, res) => {
     try {
         const { id } = req.params;
@@ -125,6 +135,113 @@ export const getCarImage = async (req, res) => {
             success: true,
             data: car.image
         });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+}
+
+export const searchCars = async (req, res) => {
+    try {
+        const { search, priceRange, category, sortBy = 'name', page = 1, limit = 15 } = req.query;
+
+        const query = {};
+
+        if (search) {
+            query.$or = [
+                {
+                    name: {
+                        $regex: search,
+                        $options: 'i'
+                    }
+                },
+                {
+                    category: {
+                        $regex: search,
+                        $options: 'i'
+                    }
+                }
+            ]
+        }
+
+        if (category) {
+            query.category = category;
+        }
+
+        if (priceRange) {
+            switch (priceRange) {
+                case 'under50':
+                    query.price = { $lt: 50 };
+                    break;
+                case '50to100':
+                    query.price = { $gte: 50, $lt: 100 };
+                    break;
+                case 'over100':
+                    query.price = { $gte: 100 };
+                    break;
+            }
+        }
+
+        let sortOptions = {}
+
+        switch (sortBy) {
+            case 'price-low':
+                sortOptions = { price: 1 };
+                break;
+            case 'price-high':
+                sortOptions = { price: -1 };
+                break;
+            default:
+                sortOptions: { name: 1 };
+        }
+
+        const skip = (page - 1) * limit;
+
+        const cars = await Car.find(query)
+            .sort(sortOptions).
+            skip(skip).
+            limit(limit);
+
+        const totalItems = await Car.countDocuments(query);
+        const categories = await Car.distinct('category', query);
+        const brands = await Car.distinct('brand', query);
+
+        const carsData = cars.map(car => {
+            return {
+                id: car._id,
+                name: car.name,
+                category: car.category,
+                brand: car.brand,
+                model: car.model,
+                year: car.year,
+                price: car.price,
+                image: car.image,
+                features: car.features,
+                transmission: car.transmission,
+                fuelType: car.fuelType,
+                seats: car.seats,
+                Avaible: car.isAvaible
+            }
+        });
+
+        return res.status(200).json({
+            success: true,
+            data: carsData,
+            categories: categories,
+            brands: brands,
+            pagination: {
+                page: parseInt(page),
+                totalPages: Math.ceil(totalItems / limit),
+                totalItems: totalItems,
+                limit: parseInt(limit)
+            }
+        });
+
 
     } catch (error) {
         console.log(error);
