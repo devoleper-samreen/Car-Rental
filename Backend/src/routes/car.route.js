160e2a472ed61976a86e2317468ae52ad6dcecec
devoleper-samreen <samreenmalik52292@gmail.com>
2025-05-09 15:07:04 +0530

    feat: add image upload functionality to car addition route

--- Backend/src/routes/car.route.js
+++ Backend/src/routes/car.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { addCar, getCarById, getCarImage, searchCars } from '../controllers/car.controller.js';
 import { verifyAdmin } from '../middleware/verifyAdmin.js';
+import { upload } from '../utils/multer.js';
 
 const router = express.Router();
 
-router.post('/add', verifyAdmin, addCar);
+router.post('/add', verifyAdmin, upload.single('image'), addCar);
 router.get('/:id', getCarById);
 router.get('/image/:id', getCarImage);
 router.get('/search', searchCars);
