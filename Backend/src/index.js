d6d6d0e9e3998f87236803a6d68d710834f6a207
devoleper-samreen <samreenmalik52292@gmail.com>
2025-05-09 13:49:35 +0530

    feat: enhance CORS configuration and add uploads static path

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import dotenv from 'dotenv';
 import { connectDB } from './utils/db.js';
 import userRoutes from './routes/user.route.js';
@@ -12,7 +13,12 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: ['*'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
 app.get('/', (req, res) => {
