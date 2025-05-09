2dd175409f5b253f35f6bed531ee64f0b1bfcc07
devoleper-samreen <samreenmalik52292@gmail.com>
2025-05-09 13:52:29 +0530

    feat: add car model schema with validation and default values

---
+++ Backend/src/models/car.model.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose'
+
+const carSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add car name']
+    },
+    category: {
+        type: String,
+        enum: ['hatchback', 'sedan', 'suv'],
+        required: true
+    },
+    brand: {
+        type: String,
+        required: [true, 'Please add brand name']
+    },
+    model: {
+        type: String,
+        required: [true, 'Please add model']
+    },
+    year: {
+        type: Number,
+        required: [true, 'Please add year']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please add price per day']
+    },
+    image: {
+        type: String,
+        required: [true, 'Please add car image']
+    },
+    features: [{
+        type: String
+    }],
+    transmission: {
+        type: String,
+        enum: ['automatic', 'manual'],
+        require: true
+    },
+    fuelType: {
+        type: String,
+        enum: ['petrol', 'disel', 'electric', 'hybrid'],
+        required: true
+    },
+    seats: {
+        type: Number,
+        required: true
+    },
+    isAvaible: {
+        type: Boolean,
+        default: true
+    }
+}, { timestamps: true }
+)
+
+export const Car = mongoose.model('Car', carSchema);
