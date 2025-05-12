c33878b9036ec42e95b6d0cbe3f266aa763ff20c
devoleper-samreen <samreenmalik52292@gmail.com>
2025-05-12 10:06:39 +0530

    feat: enhance payment confirmation with detailed responses and status handling

--- Backend/src/controllers/payment.controller.js
+++ Backend/src/controllers/payment.controller.js
@@ -3,7 +3,6 @@ import { stripe } from '../utils/stripe.js'
 
 
 //create payment intent
-
 export const createPaymentIntent = async (req, res) => {
     const { bookingId } = req.body
 
@@ -52,9 +51,51 @@ export const confirmPayment = async (req, res) => {
             })
         }
 
+        const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId)
 
+        switch (paymentIntent.status) {
+            case 'succeeded':
+                const booking = await Booking.findById(bookingId)
+                    .populate('carId')
+                    .populate('userId')
 
+                booking.status = 'confirmed'
+                await booking.save()
+
+                const recipt = await generateRecipt(booking, paymentIntent)
+
+                return res.status(200).json({
+                    success: true,
+                    message: 'Payment confirmed successfully',
+                    booking,
+                    recipt
+                })
+
+            case 'processing':
+                return res.status(202).json({
+                    success: true,
+                    message: 'Your payment is still processing'
+                })
+
+            case 'requires_payment_method':
+                return res.status(400).json({
+                    success: false,
+                    message: 'Payment failed'
+                })
+
+            default:
+                return res.status(400).json({
+                    success: false,
+                    message: `Payment failed with status: ${paymentIntent.status}. Please try again`
+                })
+
+        }
     } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Something went wrong'
+        })
 
     }
 }
