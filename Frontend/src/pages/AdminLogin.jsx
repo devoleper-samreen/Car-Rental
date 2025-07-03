9d2fb8023423392d86679e51a71e60670a93b3b4
devoleper-samreen <samreenmalik52292@gmail.com>
2025-07-03 11:25:09 +0530

    feat: refactor Admin and User login components for improved code readability and maintainability

--- Frontend/src/pages/AdminLogin.jsx
+++ Frontend/src/pages/AdminLogin.jsx
@@ -1,73 +1,95 @@
-import { FaUserShield, FaEnvelope, FaLock } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import AxiosInstance from '../apiManager/axiosInstance';
-import { adminLogin } from "../features/auth/authSlice"
+import { FaUserShield, FaEnvelope, FaLock } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AxiosInstance from "../apiManager/axiosInstance";
+import { adminLogin, userLogout } from "../features/auth/authSlice";
 import { useDispatch } from "react-redux";
 import toast from "react-hot-toast";
 
 const AdminLoginPage = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const fromData = {
-        email: email,
-        password: password
+  const fromData = {
+    email: email,
+    password: password,
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await AxiosInstance.post("/api/admin/login", fromData);
+
+      console.log(response);
+
+      dispatch(
+        adminLogin({
+          user: response.data.admin,
+          adminToken: response.data.admin?.accessToken,
+        })
+      );
+
+      dispatch(userLogout());
+
+      toast.success(response.data.message);
+      navigate("/admin/dashboard");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
     }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-
-        try {
-            const response = await AxiosInstance.post("/api/admin/login", fromData);
-
-            console.log(response);
-
-            dispatch(adminLogin({
-                user: response.data.admin,
-                adminToken: response.data.admin?.accessToken
-            }))
-
-            toast.success(response.data.message);
-            navigate("/admin/dashboard");
-
-        } catch (error) {
-            console.log(error);
-            toast.error(error.response.data.message);
-        }
-    }
-    return (
-        <div className="min-h-screen bg-[#0f172a] flex items-center justify-center">
-            <div className="bg-white p-10 rounded-2xl shadow-2xl w-full max-w-md">
-                <div className="flex justify-center mb-4">
-                    <FaUserShield size={40} className="text-violet-600" />
-                </div>
-                <h2 className="text-2xl font-bold text-center mb-6 text-gray-900">Admin Login</h2>
-                <form className="space-y-6" onSubmit={handleSubmit}>
-                    <div className="flex items-center border px-4 py-2 rounded-md">
-                        <FaEnvelope className="text-gray-400 mr-3" />
-                        <input className="w-full outline-none" type="email" placeholder="Admin Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <div className="flex items-center border px-4 py-2 rounded-md">
-                        <FaLock className="text-gray-400 mr-3" />
-                        <input className="w-full outline-none" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    </div>
-                    <button className="w-full bg-violet-600 hover:bg-violet-700 text-white py-2 rounded-md font-semibold transition duration-300 cursor-pointer" type='submit'>
-                        Admin Sign In
-                    </button>
-                </form>
-                <div className='flex mt-6 items-center justify-center w-full'>
-                    <Link to="/admin/signup" className="text-sm text-violet-600 hover:underline cursor-pointer ">
-                        Register new admin
-                    </Link>
-                </div>
-
-            </div>
+  };
+  return (
+    <div className="min-h-screen bg-[#0f172a] flex items-center justify-center">
+      <div className="bg-white p-10 rounded-2xl shadow-2xl w-full max-w-md">
+        <div className="flex justify-center mb-4">
+          <FaUserShield size={40} className="text-violet-600" />
         </div>
-    );
+        <h2 className="text-2xl font-bold text-center mb-6 text-gray-900">
+          Admin Login
+        </h2>
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div className="flex items-center border px-4 py-2 rounded-md">
+            <FaEnvelope className="text-gray-400 mr-3" />
+            <input
+              className="w-full outline-none"
+              type="email"
+              placeholder="Admin Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center border px-4 py-2 rounded-md">
+            <FaLock className="text-gray-400 mr-3" />
+            <input
+              className="w-full outline-none"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            className="w-full bg-violet-600 hover:bg-violet-700 text-white py-2 rounded-md font-semibold transition duration-300 cursor-pointer"
+            type="submit"
+          >
+            Admin Sign In
+          </button>
+        </form>
+        <div className="flex mt-6 items-center justify-center w-full">
+          <Link
+            to="/admin/signup"
+            className="text-sm text-violet-600 hover:underline cursor-pointer "
+          >
+            Register new admin
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminLoginPage;
--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,66 +1,86 @@
 import { FaEnvelope, FaLock } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import AxiosInstance from "../apiManager/axiosInstance"
+import AxiosInstance from "../apiManager/axiosInstance";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { useState } from "react";
-import { userLogin } from "../features/auth/authSlice"
+import { userLogin, adminLogout } from "../features/auth/authSlice";
 import { useDispatch } from "react-redux";
 
 const SignIn = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const fromData = {
-        email: email,
-        password: password
+  const fromData = {
+    email: email,
+    password: password,
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await AxiosInstance.post("/api/user/login", fromData);
+
+      console.log(response);
+
+      dispatch(
+        userLogin({
+          user: response.data.userData,
+          userToken: response.data.userData.acessToken,
+        })
+      );
+
+      dispatch(adminLogout());
+
+      toast.success(response.data.message);
+      navigate("/list");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
     }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-
-        try {
-            const response = await AxiosInstance.post("/api/user/login", fromData);
-
-            console.log(response);
-
-            dispatch(userLogin({
-                user: response.data.userData,
-                userToken: response.data.userData.acessToken
-            }))
-
-            toast.success(response.data.message);
-            navigate("/list");
-
-        } catch (error) {
-            console.log(error);
-            toast.error(error.response.data.message);
-        }
-
-    }
-    return (
-        <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-purple-500 to-indigo-600">
-            <div className="bg-white p-8 rounded-2xl w-[400px]">
-                <h2 className="text-2xl font-bold text-center mb-6">Welcome Back</h2>
-                <form className="space-y-6" onSubmit={handleSubmit}>
-                    <div className="flex items-center bg-gray-100 rounded px-3 py-2">
-                        <FaEnvelope className="text-gray-400 mr-3" />
-                        <input type="email" placeholder="Email Address" className="bg-transparent w-full outline-none" onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <div className="flex items-center bg-gray-100 rounded px-3 py-2">
-                        <FaLock className="text-gray-400 mr-3" />
-                        <input type="password" placeholder="Password" className="bg-transparent w-full outline-none" onChange={(e) => setPassword(e.target.value)} />
-                    </div>
-                    <button className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition cursor-pointer" type="submit">Sign In</button>
-                </form>
-                <p className="text-center text-sm mt-4 text-gray-600">
-                    Don’t have an account? <Link to="/user/signup" className="text-indigo-600 hover:underline">Sign Up</Link>
-                </p>
-            </div>
-        </div>
-    );
+  };
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-purple-500 to-indigo-600">
+      <div className="bg-white p-8 rounded-2xl w-[400px]">
+        <h2 className="text-2xl font-bold text-center mb-6">Welcome Back</h2>
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div className="flex items-center bg-gray-100 rounded px-3 py-2">
+            <FaEnvelope className="text-gray-400 mr-3" />
+            <input
+              type="email"
+              placeholder="Email Address"
+              className="bg-transparent w-full outline-none"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center bg-gray-100 rounded px-3 py-2">
+            <FaLock className="text-gray-400 mr-3" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="bg-transparent w-full outline-none"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition cursor-pointer"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </form>
+        <p className="text-center text-sm mt-4 text-gray-600">
+          Don’t have an account?{" "}
+          <Link to="/user/signup" className="text-indigo-600 hover:underline">
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
